b6856c9a561dd75b15d92561de8f218df02d6458
Guilherme Rodrigues <gadr90@gmail.com>
2013-04-07 20:12:17 -0300

    Style refactoring

--- src/DigitalSignature.java
+++ src/DigitalSignature.java
@@ -4,47 +4,37 @@ import java.security.*;
 
 public class DigitalSignature {
 
+    public static final String ENCODING = "UTF-8";
+
+    public static MessageDigest getMD5() throws NoSuchAlgorithmException {
+        return MessageDigest.getInstance("MD5");
+    }
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) throws Exception {
-		
-	    // verify args
-	    if (args.length !=1) {
-	      System.err.println("Usage: java DigitalSignatureExample text");
-	      System.exit(1);
-	    }
-	    // get plain text
-	    byte[] plainText = args[0].getBytes("UTF8");
-	    
-	    // Digest code sample
-	    
-	    // get a message digest object using the MD5 algorithm
-	    MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+        MessageDigest md5;
+        byte[] plainText, digest;
 
-	    // print out the provider used
-	    System.out.println( "\n" + messageDigest.getProvider().getInfo() );
-	    
-	    // calculate the digest and print it out
-	    messageDigest.update(plainText);
-	    byte [] digest = messageDigest.digest();
-	    System.out.println( "\nDigest length: " + digest.length * 8 + "bits" );
+        // verify args
+        if (args.length !=1) {
+            System.err.println("Usage: java DigitalSignatureExample text");
+            System.exit(1);
+        }
+        // get plain text
+        plainText = args[0].getBytes(ENCODING);
 
-	    // convert the digest to hex
-	    StringBuffer buf = new StringBuffer();
-	    for(int i = 0; i < digest.length; i++) {
-	       String hex = Integer.toHexString(0x0100 + (digest[i] & 0x00FF)).substring(1);
-	       buf.append((hex.length() < 2 ? "0" : "") + hex);
-	    }
+        // get a message digest object using the MD5 algorithm
+        md5 = getMD5();
 
-	    // print the digest in hex
-	    System.out.println( "\nDigest(hex): " );
-	    System.out.println( buf.toString() );	   	   
-	    
-	    // end digest sample
-	    
-	    
-	    
+        // calculate the digest and print it out
+        md5.update(plainText);
+        digest = md5.digest();
+
+        System.out.println("\n" + md5.getProvider().getInfo());
+        System.out.println( "\nDigest length: " + digest.length * 8 + "bits" );
+	    System.out.println( "\nDigest(hex): "  + Hex.encodeHexString(digest));
+
 	    // generate RSA's key pair
 	    System.out.println( "\nStart generating RSA key" );
 	    KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
